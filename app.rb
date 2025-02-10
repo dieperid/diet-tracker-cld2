72c9420cef7dae9715b7ab8a5134110e8db94665
David Dieperink <david.dieperink@ik.me>
2025-02-10 10:48:50 +0100

    feat: added Cloudinary support for images

--- app.rb
+++ app.rb
@@ -8,15 +8,25 @@ MIDDLEWARES_FOLDER = './app/middlewares' unless defined?(MIDDLEWARES_FOLDER)
 CONFIG_FOLDER = './config' unless defined?(CONFIG_FOLDER)
 
 require 'sinatra'
-require 'dotenv/load'
+require 'dotenv'
 require 'securerandom'
 require './app/helpers/auth_helper'
 require 'chartkick'
+require 'cloudinary'
 require "#{SERVICE_FOLDER}/db_connection"
+
+Dotenv.load
 
 set :environment, ENV.fetch('RACK_ENV')
 set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
 set :views, File.join(VIEWS_FOLDER)
+
+Cloudinary.config do |config|
+  config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
+  config.api_key = ENV['CLOUDINARY_API_KEY']
+  config.api_secret = ENV['CLOUDINARY_API_SECRET']
+  config.secure = true
+end
 
 DBConnection.instance
 helpers AuthHelper
