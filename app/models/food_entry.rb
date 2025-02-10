f0b01556d30b8147d68a6284d0d9bdfb88408d01
David Dieperink <david.dieperink@ik.me>
2025-02-10 11:15:12 +0100

    refactor: removed presence true for the public attributes

--- app/models/food_entry.rb
+++ app/models/food_entry.rb
@@ -4,7 +4,7 @@ class FoodEntry < ActiveRecord::Base
   has_many :consumptions
 
   # Validations
-  validates :name, :carbs, :proteins, :fats, :public, presence: true
+  validates :name, :carbs, :proteins, :fats, presence: true
   validates :calories, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :image, presence: true # format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]) }
 end
