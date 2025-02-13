9d5f58923123716e785c9027d30198884f2f3f0e
David Dieperink <david.dieperink@ik.me>
2025-02-13 10:52:38 +0100

    build: added `bundle install` command and export .env variable

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 set -o errexit
 
+export $(grep -v '^#' .env | xargs)
+
 echo "Generating config/database.yml..."
 
-cat <<EOT >config/database2.yml
+cat <<EOT >config/database.yml
 production:
   adapter: mysql2
   encoding: utf8mb4
   reconnect: true
-  url: <%= ENV['DATABASE_URL'] %>
+  database: ${DATABASE_NAME}
+  pool: 5
+  username: ${DATABASE_USERNAME}
+  password: ${DATABASE_PASSWORD}
+  host: ${DATABASE_HOST}
+  port: ${DATABASE_PORT}
 EOT
+
+bundle install
